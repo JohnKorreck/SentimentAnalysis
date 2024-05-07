5240a924bd5297ea732c8d3062050ad2e928fdfb
Luke Zhu <lezhu221@gmail.com>
2024-05-07 17:57:02 -0400

    test

--- scraper.py
+++ scraper.py
@@ -1,4 +1,22 @@
+import math
+from scipy.stats import norm
+import requests
+from urllib.parse import quote
+import os
+from dotenv import load_dotenv
+from bs4 import beuatifulsoup
+
+def webscraper ():
+
+
+
+
+
 def main():
     numbers = [1,2,3]
     print("hello ")
+
+
+
+
     
