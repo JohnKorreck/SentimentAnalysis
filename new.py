de9e3a8d5918fea12ef084151a084683d3d113b0
John Korreck <johnkorreck3@gmail.com>
2024-05-13 16:47:28 -0400

    first draft of model comparison

--- new.py
+++ new.py
@@ -1,7 +1,14 @@
-l = 0
-nums = [1,2,3,0,0]
-for r in range(len(nums)):
-    if nums[r]:
-        nums[l], nums[r] = nums[r], nums[l]
-        l += 1
-print(nums)
+from transformers import pipeline
+from transformers import AutoTokenizer,AutoModelForSequenceClassification
+
+model_name = "mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+res = classifier("10. Disney's Future Is Brighter Than It Appears Investors sold off Disney's stock, but the company is still growing in the right places. ")
+
+
+
+print (res)
--- scraper.py
+++ scraper.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
-
+import torch
 import requests
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
+import numpy as np
+import pandas as pd
+from transformers import pipeline
+from transformers import AutoTokenizer,AutoModelForSequenceClassification
 
 def get_article_header_list(ticker):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
     url = f'https://finance.yahoo.com/quote/{ticker}/news'
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
@@ -22,10 +26,31 @@ def new_file_info(textfile, ticker):
     f = open(textfile, 'w')
     num = 1
     for header in get_article_header_list(ticker):
-        f.write(f'{num}. {header}\n\n')
+        f.write(f'{header}\n')
         num += 1
     f.close()
 
+def ticker_sentiment(textfile):
+    model_name = "mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    f = open(textfile, 'r')
+    lines = f.readlines()
+    score = 0
+    for header in lines:
+        res = classifier(header)
+        if res[0]['label'] == 'positive':
+            score += 1
+        elif res[0]['label'] == 'negative':
+            score -= 1
+        else:
+            pass
+    f.close()
+    return score
+
 ticker = input('Enter a ticker symbol: ')
 
-new_file_info('articleheadline.txt', ticker)
+new_file_info(f'{ticker}-headlines.txt', ticker)
+
+print(ticker_sentiment(f'{ticker}-headlines.txt'))
